a2c38be46f431c5c1bd6315e0898b1cbafbff537
Thies, Santo <s.thies@campus.lmu.de>
2024-05-16 11:47:16 +0200

    update

--- build.zsh
+++ build.zsh
@@ -4,5 +4,6 @@ if [ "$(git rev-parse --abbrev-ref HEAD)" = "main" ]; then
   echo "Publishing from main branch"
   ghp-import -n -p -f _build/html
 else
-  echo "Not Publishing changes. Only allowed from main branch!!!!"
+  echo "Not Publishing changes. Changes are only locally visible."
+  echo "If you want to publish to website, please switch to main branch and merge your changes into it."
 fi
