1df8df9dfc8a3c2a1b5d49cc2c326903926144f8
Thies, Santo <s.thies@campus.lmu.de>
2024-05-16 11:45:02 +0200

    update

--- build.zsh
+++ build.zsh
@@ -3,4 +3,6 @@ jupyter-book build .
 if [ "$(git rev-parse --abbrev-ref HEAD)" = "main" ]; then
   echo "Publishing from main branch"
   ghp-import -n -p -f _build/html
+else
+  echo "Not Publishing changes. Only allowed from main branch!!!!"
 fi
