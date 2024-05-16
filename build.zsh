b9f0769de539008bd96de187df1a591465f3a94e
Thies, Santo <s.thies@campus.lmu.de>
2024-05-16 11:43:50 +0200

    update

--- build.zsh
+++ build.zsh
@@ -1,6 +1,6 @@
 jupyter-book clean .
 jupyter-book build .
 if [ "$(git rev-parse --abbrev-ref HEAD)" = "main" ]; then
-  echo "Publishing to main branch"
+  echo "Publishing from main branch"
   ghp-import -n -p -f _build/html
 fi
